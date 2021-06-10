a0e69ee0e5c9b740ef081eb204315cfbeb8ea6a4
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-06-10 16:50:02 -0400

    commit tree 928c92fc3b742dd412abf709417040b605eba1dc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, name) {
+    let thankYouCards = []
+    for (let i = 0; i < array.length; i++) {
+      thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+    }
+  
+    return thankYouCards;
+  }
+
+  function countDown(i) {
+    while (i >= 0) {
+        console.log(i)
+        i -= 1
+    }
+    return i
+  }
